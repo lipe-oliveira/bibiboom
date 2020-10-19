1308d8d9241ff5af339bdcd7ea9fcffad21063a2
felip <felipeoliveira.b@gmail.com>
2020-10-18 21:01:13 -0300

    auto

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -212,8 +212,9 @@ router.post('/post_restaurantes_dono', async (req, res) => {
 	try {
 		const { id, usuario, senha } = req.body;
 		if (await Restaurante.findOne({ id })) {
-			let restaurante = await Restaurante.findOne({ id });
-		
+			const restaurante = await Restaurante.findOne({ id });
+			console.log(restaurante);
+			
 			let dono = {
 				usuario,
 				senha
