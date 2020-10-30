b70a75a8f2e1689ca15ca5bf1a5d12fa63c72b85
felip <felipeoliveira.b@gmail.com>
2020-10-30 19:56:34 -0300

    auto

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -67,7 +67,7 @@ router.get('/get_feeds/:id', async (req, res) => {
 router.get('/get_users', async (req, res) => {
 	try {
 		const user = await User.find();
-		return res.send({ user }).populate("salvos");
+		return res.send({ user }).populate("salvos.estabelecimento");
 	} catch (err) {}
 });
 
